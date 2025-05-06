086bb2850817404aeabddd39e1a0ce20b1437b5b
Xu Lai <58556678+PeopleWhoListenToStories@users.noreply.github.com>
2025-05-06 17:36:21 +0800

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -38,15 +38,15 @@ const rewrites = async () => {
     },
     {
       source: '/slaykit/:slug*',
-      destination: 'http://110.40.181.43:31062/:slug*',
+      destination: 'https://api.sanxi.xulai.fun/ws-sanxi/:slug*',
     },
     {
       source: '/zh-CN/slaykit/:slug*',
-      destination: 'http://110.40.181.43:31062/:slug*',
+      destination: 'https://api.sanxi.xulai.fun/ws-sanxi/:slug*',
     },
     {
       source: '/en/slaykit/:slug*',
-      destination: 'http://110.40.181.43:31062/:slug*',
+      destination: 'https://api.sanxi.xulai.fun/ws-sanxi/:slug*',
     },
   ];
 };
@@ -72,8 +72,8 @@ const nextConfig: NextConfig = {
   rewrites,
   env: {
     SERVER_API_URL: '/slaykit',
-    // COLLABORATION_API_URL: 'https://api.sanxi.xulai.fun/wss/',
-    COLLABORATION_API_URL: 'wss://110.40.181.43:31063/',
+    COLLABORATION_API_URL: 'https://api.sanxi.xulai.fun/ws-sanxi/',
+    // COLLABORATION_API_URL: 'wss://110.40.181.43:31063/',
     NEXT_PUBLIC_LOCALE_PREFIX: 'always',
     NEXT_PUBLIC_TIPTAP_AI_APP_ID: '79drg0x6',
     NEXT_PUBLIC_TIPTAP_AI_TOKEN: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTk5MDQxNjl9.lYbRIEGpK2k5J8cRY-MEnx65gMn4uNx8YjUfBmGyn08',
