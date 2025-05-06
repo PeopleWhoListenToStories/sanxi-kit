11a4436460e3d7303b4b13b3641bc62dfde4bdae
Xu Lai <58556678+PeopleWhoListenToStories@users.noreply.github.com>
2025-05-06 17:55:11 +0800

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -38,15 +38,15 @@ const rewrites = async () => {
     },
     {
       source: '/slaykit/:slug*',
-      destination: 'https://api.sanxi.xulai.fun/api-sanxi/:slug*',
+      destination: 'http://110.40.181.43:31062/:slug*',
     },
     {
       source: '/zh-CN/slaykit/:slug*',
-      destination: 'https://api.sanxi.xulai.fun/api-sanxi/:slug*',
+      destination: 'http://110.40.181.43:31062/:slug*',
     },
     {
       source: '/en/slaykit/:slug*',
-      destination: 'https://api.sanxi.xulai.fun/api-sanxi/:slug*',
+      destination: 'http://110.40.181.43:31062/:slug*',
     },
   ];
 };
