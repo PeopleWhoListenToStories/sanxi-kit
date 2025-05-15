1483efce6faafc263798b69162bb626640731297
xulai <xulai.me@foxmail.com>
2025-05-15 15:23:56 +0800

    ✨ Feat: add clarity

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,6 +15,19 @@ export default function RootLayout({ children }: Props) {
     <html lang="zh-CN" className={inter.className}>
       <head>
         <Script defer src="https://umami.xulai.fun/script.js" data-website-id="4e405d27-c14b-4799-8fd0-31a7b250c32b" />
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "rju0hq0fdx");
+            `
+          }}
+        />
       </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="enableSystem" enableSystem={true}>
